d3e9057d476b70c82fbb63e2086ba50b9b2ac1c2
Drew Taylor <drewtaylorm@gmail.com>
2025-02-23 15:30:29 -0500

    sync formValues with item and fix some layout quirks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     // bg-teal-400
     return (
         <header>
-            <h1 className="text-center text-white text-3xl p-4 font-bold bg-teal-400">
+            <h1 className="text-center text-white text-6xl lg:text-3xl p-4 font-bold bg-teal-400">
                 dutch
             </h1>
         </header>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,5 @@
 import { useDrop } from "react-dnd";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //  & {updateItemPeople: (id: string, person: PersonProps) => undefined}
 
@@ -15,6 +15,8 @@ function Item({ item, onUpdateItem, onRemoveItem }) {
         cost: item.cost,
         people: item.people,
     });
+
+    useEffect(() => setFormValues(item), [item]);
 
     const [hidden, setHidden] = useState(true);
 
@@ -67,9 +69,9 @@ function Item({ item, onUpdateItem, onRemoveItem }) {
                 id={`${item.id}-form`}
                 className={`${hidden ? "hidden " : ""}z-10 absolute h-full w-full bg-neutral-900/90`}
             >
-                <div className="bg-slate-200 w-fit m-auto rounded-lg p-1">
+                <div className="bg-slate-200 max-w-4/5 m-auto rounded-lg p-1">
                     <button
-                        className="text-sm font-bold text-slate-800/80 w-full text-right px-2"
+                        className="font-bold text-slate-800/80 w-full text-right px-2"
                         onClick={() => {
                             setFormValues(item);
                             setHidden(true);
@@ -78,39 +80,94 @@ function Item({ item, onUpdateItem, onRemoveItem }) {
                         x
                     </button>
                     <form className="flex flex-col" onSubmit={handleSubmit}>
-                        <label className="font-bold" htmlFor="name">
-                            Name:
-                        </label>
-                        <input
-                            className="rounded-lg bg-white w-fit p-1 m-1"
-                            type="text"
-                            name="name"
-                            id={`${item.id}-name`}
-                            value={formValues.name}
-                            onChange={handleChange}
-                        />
-                        <label className="font-bold" htmlFor="quantity">
-                            Quantity:
-                        </label>
-                        <input
-                            className="rounded-lg bg-white w-fit p-1 m-1"
-                            type="text"
-                            name="quantity"
-                            id={`${item.id}-quantity`}
-                            value={formValues.quantity}
-                            onChange={handleChange}
-                        />
-                        <label className="font-bold" htmlFor="cost">
-                            Cost:
-                        </label>
-                        <input
-                            className="rounded-lg bg-white w-fit p-1 m-1"
-                            type="text"
-                            name="cost"
-                            id={`${item.id}-cost`}
-                            value={formValues.cost}
-                            onChange={handleChange}
-                        />
+                        <div className="grid lg:grid-cols-2">
+                            <div className="flex w-2/3">
+                                <label
+                                    className="font-bold flex-1 m-auto"
+                                    htmlFor="name"
+                                >
+                                    Name:
+                                </label>
+                                <input
+                                    className="rounded-lg bg-white flex-3 p-1 m-1"
+                                    type="text"
+                                    name="name"
+                                    id={`${item.id}-name`}
+                                    value={formValues.name}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="flex w-2/3">
+                                <label
+                                    className="font-bold flex-1 m-auto"
+                                    htmlFor="quantity"
+                                >
+                                    Quantity:
+                                </label>
+                                <input
+                                    className="rounded-lg bg-white flex-3 p-1 m-1"
+                                    type="text"
+                                    name="quantity"
+                                    id={`${item.id}-quantity`}
+                                    value={formValues.quantity}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="flex w-2/3">
+                                <label
+                                    className="font-bold flex-1 m-auto"
+                                    htmlFor="cost"
+                                >
+                                    Cost:
+                                </label>
+                                <input
+                                    className="rounded-lg bg-white flex-3 p-1 m-1"
+                                    type="text"
+                                    name="cost"
+                                    id={`${item.id}-cost`}
+                                    value={formValues.cost}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <label
+                                className="font-bold flex-1 m-auto"
+                                htmlFor="people"
+                            >
+                                People:
+                            </label>
+                            <div
+                                id={`${item.id}-people}`}
+                                className="rounded-lg bg-white flex flex-wrap flex-3 justify-around m-auto lg:w-2/3"
+                            >
+                                {formValues.people.map((person) => {
+                                    return (
+                                        <div
+                                            id={person.id}
+                                            className="rounded-lg bg-teal-100 flex w-fit p-1 m-1"
+                                        >
+                                            {person.name}
+                                            <div
+                                                className="text-slate-500 font-bold w-fit mx-2"
+                                                onClick={() => {
+                                                    setFormValues({
+                                                        ...formValues,
+                                                        people: formValues.people.filter(
+                                                            (p) =>
+                                                                p.id !==
+                                                                person.id,
+                                                        ),
+                                                    });
+                                                }}
+                                            >
+                                                x
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
                         <div className="flex justify-between">
                             <input
                                 className="rounded-lg border-2 bg-white font-bold w-fit p-1 m-1"
